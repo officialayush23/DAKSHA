e28bf352064341dd7eb1f4f3b3a169dd5059b768
officialayush23 <officialayushp123@gmail.com>
2026-02-22 17:02:51 +0530

    j

--- BACKEND/app/api/routers/products.py
+++ BACKEND/app/api/routers/products.py
@@ -99,7 +99,7 @@ def product_detail(
         .all()
     )
     session = get_active_session(
-        db, user.id, ChannelEnum.web
+        db, user_id = user.id, channel =ChannelEnum.web
     )
     
     emit_event(
